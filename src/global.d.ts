82bfdcc44f8f5ddfbbde57c50430981035c0fdf2
cqhaisenbao <fanyu9353@gmail.com>
2022-03-07 21:13:25 +0800

    feat: 全局type

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,1 +1,22 @@
 declare module "*.scss";
+
+type Children = "Banner" | "Footer" | "List";
+
+interface ChildrenItem {
+  id: number;
+  name?: Children;
+  attributes?: {
+    title?: string;
+    description?: string;
+    avatar?: string;
+    [key: string]: any;
+  };
+}
+
+interface PageSchema {
+  name: "Page";
+  children: ChildrenItem[];
+  attributes: {
+    [key: string]: any;
+  };
+}
