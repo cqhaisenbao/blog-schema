5a5b88b5249c8962be2c536be3ad097a16ac2878
cqhaisenbao <fanyu9353@gmail.com>
2022-03-07 21:23:46 +0800

    feat: reducer 常量

---
+++ src/admin/Page/HomeManagement/store/constant.ts
@@ -0,0 +1,7 @@
+const nameSpace = "HOME_MANAGEMENT_PAGE";
+
+export const RESET_SCHEMA = `${nameSpace}/RESET_SCHEMA`;
+export const ADD_PAGE_CHILDREN = `${nameSpace}/ADD_PAGE_CHILDREN`;
+export const CHANGE_PAGE_CHILDREN = `${nameSpace}/CHANGE_PAGE_CHILDREN`;
+export const DELETE_PAGE_CHILDREN = `${nameSpace}/DELETE_PAGE_CHILDREN`;
+export const SORT_PAGE_CHILDREN = `${nameSpace}/SORT_PAGE_CHILDREN`;
