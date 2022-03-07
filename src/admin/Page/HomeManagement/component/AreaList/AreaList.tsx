cf286dd78528f04a10b531507d1ee7cd152e4941
cqhaisenbao <fanyu9353@gmail.com>
2022-03-07 22:00:56 +0800

    feat: list 样式

--- src/admin/Page/HomeManagement/component/AreaList/AreaList.tsx
+++ src/admin/Page/HomeManagement/component/AreaList/AreaList.tsx
@@ -1,11 +1,19 @@
 import { Button, Divider } from "antd";
 import useHomeManagementStore from "../../hooks/useHomeManagementStore";
+import AreaListItem from "../AreaListItem";
+import styles from "./style.module.scss";
 
 const AreaList = () => {
   const { schema, addPageChildren } = useHomeManagementStore();
+  const { children = [] } = schema;
 
   return (
     <div>
+      <ul className={styles.list}>
+        {children.map((item) => (
+          <AreaListItem item={item} key={item.id} />
+        ))}
+      </ul>
       <Button type="primary" ghost onClick={addPageChildren}>
         新增页面区块
       </Button>
